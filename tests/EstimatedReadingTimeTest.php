cff36291d5d5fcda847a1f573338f906153b2942
midesweb <malvarez@desarrolloweb.com>
2025-06-26 12:19:34 +0200

    add french to available languages

--- tests/EstimatedReadingTimeTest.php
+++ tests/EstimatedReadingTimeTest.php
@@ -17,15 +17,39 @@ class EstimatedReadingTimeTest extends TestCase
   }
 
   #[Test]
+  public function itGetCorrectReadTimeInEnglish()
+  {
+    $content = str_repeat("palabra ", 250);
+    $estimator = new EstimatedReadingTime($content);
+
+    $readTime = $estimator->getReadTime();
+
+    $this->assertStringContainsString('minutes', $readTime);
+    $this->assertStringContainsString('2', $readTime);
+  }
+
+  #[Test]
   public function itGetCorrectReadTimeInSpanish()
   {
-    $content = str_repeat("palabra ", 250); // Genera 250 palabras
+    $content = str_repeat("palabra ", 25000);
     $estimator = new EstimatedReadingTime($content);
     $estimator->setLanguage('es');
 
     $readTime = $estimator->getReadTime();
 
-    $this->assertStringContainsString('minuto', $readTime);
-    $this->assertStringContainsString('2', $readTime);
+    $this->assertStringContainsString('horas', $readTime);
+    $this->assertStringContainsString('3', $readTime);
+  }
+
+  #[Test]
+  public function itGetCorrectReadTimeInFrench()
+  {
+    $content = str_repeat("palabra ", 7200);
+    $estimator = new EstimatedReadingTime($content);
+    $estimator->setLanguage('fr');
+
+    $readTime = $estimator->getReadTime();
+
+    $this->assertEquals('1 heure', $readTime);
   }
 }
